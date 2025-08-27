8decbb7ce63459bb35b1ad7f1e4a76ef396fabce
gautampawar1638-cpu <gautampawar1638@gmail.com>
2025-08-27 09:47:57 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountid = 144553
+let accountemail = "hitesh@google.com"
+var accountpassord = "12345"
+accountCity = "jaipur"
+let accountstate;
+
+// accountId = 2 // not allowed
+
+accountemail = "hc@hc.com"
+accountpassword = "21212121"
+accountCity = "bengaluru"
+
+
+
+console.table([accountid, accountemail, accountpassord, accountCity, accountpassord, accountstate])
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
